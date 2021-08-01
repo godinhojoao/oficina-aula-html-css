d8b7fd95a03b82292a8d309effcba8674571637f
Joao Godinho <godinhojoao2003@gmail.com>
2021-08-01 00:46:34 -0300

    feat: link diferente de pagina atual

--- site-final/main.js
+++ site-final/main.js
@@ -4,6 +4,13 @@
 (function () {
   const toggler = document.querySelector('.main-container__header__list__toggler');
   const nav = document.querySelector('.main-container__header__navigation');
+  const allLinks = nav.children;
+
+  for (let link of allLinks) {
+    if (link.href == location.href) {
+      link.style.backgroundColor = '#222121';
+    }
+  }
 
   toggler.addEventListener('click', function () {
     nav.classList.toggle('closed');
